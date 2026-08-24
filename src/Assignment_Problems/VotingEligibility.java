844f43fcd6140dce938a512d82205a99761d21ff
Jovitha1412 <jovitha.evang@gmail.com>
2026-08-24 12:50:13 +0530

    completed voitng eligibility assignment

---
+++ src/Assignment_Problems/VotingEligibility.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class VotingEligibility {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter age: ");
+        int age=s.nextInt();
+
+        if (age>=18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
