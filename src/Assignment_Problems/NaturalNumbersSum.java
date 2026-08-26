ee911622c8a95b0ffb73f38acc5c7a40c98597a1
Jovitha1412 <jovitha.evang@gmail.com>
2026-08-26 09:28:24 +0530

    completed sum of natural no.sassignment

---
+++ src/Assignment_Problems/NaturalNumbersSum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class NaturalNumbersSum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        NaturalNumbersSum(n);
+    }
+
+    public static void NaturalNumbersSum(int n) {
+        int counter = 1;
+        int sum = 0;
+
+        while (counter <= n) {
+            sum = sum + counter;
+            counter++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+}
