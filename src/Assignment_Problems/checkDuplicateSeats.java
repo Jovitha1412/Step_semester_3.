5702045157fe9a939e3e0275f6ebd1850b3dddc2
Jovitha1412 <jovitha.evang@gmail.com>
2026-08-29 08:33:14 +0530

    week 1 s1 qn-1 Add checkDuplicateSeats S1

---
+++ src/Assignment_Problems/checkDuplicateSeats.java
@@ -0,0 +1,40 @@
+public class checkDuplicateSeats {
+
+    public static void main(String[] args) {
+        int[] seatNumbers = {101, 102, 103, 102, 105};
+
+        checkDuplicateSeats(seatNumbers);
+    }
+
+    static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+
+            boolean alreadyChecked = false;
+
+            for (int k = 0; k < i; k++) {
+                if (seatNumbers[i] == seatNumbers[k]) {
+                    alreadyChecked = true;
+                    break;
+                }
+            }
+
+            if (alreadyChecked) {
+                continue;
+            }
+
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    duplicateFound = true;
+                    break;
+                }
+            }
+        }
+
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+}
