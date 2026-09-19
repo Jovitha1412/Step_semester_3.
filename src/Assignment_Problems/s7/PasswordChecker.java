6eb48f445da200ac8a108b853764a8ce9663dd9c
Jovitha1412 <jovitha.evang@gmail.com>
2026-09-19 16:39:47 +0530

    add week 7 passwordchecker

---
+++ src/Assignment_Problems/s7/PasswordChecker.java
@@ -0,0 +1,38 @@
+
+package classproblem.s7;
+
+public class PasswordChecker {
+
+    private final String password;
+
+    // Constructor
+    public PasswordChecker(String password) {
+        this.password = password;
+    }
+
+    // Return only the strength rating
+    public String getStrength() {
+
+        int length = password.length();
+
+        if (length < 6) {
+            return "Weak";
+        } else if (length <= 9) {
+            return "Medium";
+        } else {
+            return "Strong";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PasswordChecker pc = new PasswordChecker("abcd");
+        System.out.println(pc.getStrength());
+
+        PasswordChecker pc2 = new PasswordChecker("abcdefghij");
+        System.out.println(pc2.getStrength());
+
+        PasswordChecker pc3 = new PasswordChecker("abcdefghijkl");
+        System.out.println(pc3.getStrength());
+    }
+}
