9944ff95720b76d52fed101debeb941ae1a80ce1
Jovitha1412 <jovitha.evang@gmail.com>
2026-09-19 16:33:21 +0530

    add week 7 locker

---
+++ src/classproblem/s7/Locker.java
@@ -0,0 +1,36 @@
+
+package classproblem.s7;
+
+public class Locker {
+
+    private String code;
+    private final int lockerNumber;
+
+    // Constructor
+    public Locker(int lockerNumber, String code) {
+        this.lockerNumber = lockerNumber;
+        this.code = code;
+    }
+
+    // Change the code
+    public void changeCode(String currentCode, String newCode) {
+
+        if (code.equals(currentCode)) {
+            code = newCode;
+            System.out.println("Code changed successfully");
+        } else {
+            System.out.println("Code change rejected");
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        Locker l = new Locker(101, "1234");
+
+        l.changeCode("1234", "5678");
+
+        l.changeCode("0000", "9999");
+    }
+}
+
