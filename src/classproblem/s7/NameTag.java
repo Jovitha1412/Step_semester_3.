6cc2c6b12e3e6e990b2efcaa334ac67f56319bec
Jovitha1412 <jovitha.evang@gmail.com>
2026-09-19 16:31:54 +0530

    add week 7 nametag

---
+++ src/classproblem/s7/NameTag.java
@@ -0,0 +1,29 @@
+
+package classproblem.s7;
+
+public class NameTag {
+
+    private final String firstName;
+    private final String lastName;
+
+    // Constructor
+    public NameTag(String fullName) {
+
+        String[] parts = fullName.split(" ");
+
+        firstName = parts[0];
+        lastName = parts[1];
+    }
+
+    // Return nickname
+    public String getNickname() {
+        return firstName + " " + lastName.charAt(0) + ".";
+    }
+
+    public static void main(String[] args) {
+
+        NameTag tag = new NameTag("Maria Gomez");
+
+        System.out.println(tag.getNickname());
+    }
+}
